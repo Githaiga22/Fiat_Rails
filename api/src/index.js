67a9973e3c7594abbccc8fdc5232dfb63874babe
Githaiga22 <allangithaiga5@gmail.com>
2025-11-02 00:52:49 +0300

    fix(api): add dotenv loading for environment variables

--- api/src/index.js
+++ api/src/index.js
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from 'express';
 import { config, validateConfig } from './config.js';
 import { initDatabase, closeDatabase, cleanupExpiredKeys } from './database.js';
